89d25e7c929e0a3383de6f7b69e1f66d8232347b
izabellemarianne <izabellemarianne08@gmail.com>
2024-11-26 09:13:58 -0500

    added constant to createpost viewmodel

--- src/main/java/controller/createPost/createPostViewModel.java
+++ src/main/java/controller/createPost/createPostViewModel.java
@@ -3,7 +3,11 @@ package controller.createPost;
 import controller.ViewModel;
 
 public class createPostViewModel extends ViewModel<createPostState> {
+
+    public static final String CONTENT_LABEL = "Content";
+
     public createPostViewModel(String viewName) {
+
         super("create post");
         setState(new createPostState());
     }
