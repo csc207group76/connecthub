2304d9391ed3d8b42ab21bf5cf34f1b2ce6697c3
lucas <lucasliu.liu@mail.utoronto.ca>
2024-11-26 16:44:15 -0500

    - logoutInteractor Test

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -13,10 +13,6 @@ public class LogoutInteractor implements LogoutInputBoundary {
 
     @Override
     public void logoutUser(LogoutInputData logoutInputData) {
-        // * get the userID out of the input data,
-        // * set the user to null in the DAO,
-        // * instantiate the 'LogoutOutputData', which needs to contain the userID,
-        // * tell the presenter to prepare a success view.
         final String userID = logoutInputData.getUserID();
         logoutDB.logoutUser();
         LogoutOutputData logoutOutputData = new LogoutOutputData(userID, true);
