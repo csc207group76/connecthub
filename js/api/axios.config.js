f102a26f305d52c7160c82c5d3397724332eae9a
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-27 16:46:02 -0500

    made base url enviornment variable

--- js/api/axios.config.js
+++ js/api/axios.config.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  // TODO Since backend & frontend are hosted on the same domain and not much production differentiation,
-  // this is fine. But it really should be done using env variables that differentiates development/producion/staging
-  baseURL: window.location.protocol + '//' + window.location.hostname  + '/api/v1',
+  baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:8080/api/v1",
   headers: {"ngrok-skip-browser-warning": true}
 })
