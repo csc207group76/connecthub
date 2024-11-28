2289663c3bd1187a80a4ba381df6624b2aaf65a2
cedricjizmejian <cedric.jizmejian@mail.utoronto.ca>
2024-11-28 18:12:35 -0500

    slight change

--- src/main/java/app/GetPostUseCaseFactory.java
+++ src/main/java/app/GetPostUseCaseFactory.java
@@ -1,6 +1,5 @@
 package app;
 
-import com.mongodb.client.MongoCollection;
 import controller.ViewManagerModel;
 import controller.delete_post.DeletePostController;
 import controller.homepage.HomepageController;
@@ -10,11 +9,9 @@ import controller.post.PostPresenter;
 import controller.post.PostViewModel;
 import daos.DBPostDataAccessObject;
 import daos.DBUserDataAccessObject;
-import org.bson.Document;
 import use_case.getpost.GetPostInputBoundary;
 import use_case.getpost.GetPostInteractor;
 import use_case.getpost.GetPostOutputBoundary;
-import app.DeletePostUseCaseFactory;
 import view.PostView;
 
 public class GetPostUseCaseFactory {
@@ -23,8 +20,7 @@ public class GetPostUseCaseFactory {
     }
 
     public static PostView create(ViewManagerModel viewManagerModel, PostViewModel postViewModel,
-                                  HomepageViewModel homepageViewModel, DBPostDataAccessObject postDAO,
-                                  DBUserDataAccessObject userDAO) {
+                                  HomepageViewModel homepageViewModel, DBPostDataAccessObject postDAO, DBUserDataAccessObject userDAO) {
         final PostController postController = createGetPostUseCase(viewManagerModel, postViewModel, postDAO);
         final HomepageController homepageController = HomepageUseCaseFactory.createHomepageController(viewManagerModel,
                 homepageViewModel, postViewModel, postDAO);
