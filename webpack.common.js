dbad4791fd8916f15f0154bd423dae67942693a8
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-30 01:44:54 -0500

    diff btw dev and prod for webpack

---
+++ webpack.common.js
@@ -0,0 +1,69 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const Dotenv = require("dotenv-webpack")
+
+const presets = [
+  ["@babel/preset-env", { useBuiltIns: "usage", corejs: 3 }],
+  ["@babel/preset-react", { runtime: "automatic" }],
+]
+
+const isDevelopment = process.env.NODE_ENV !== "production"
+
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  entry: {
+    "js/styles/app": "./styles/style.css",
+    "js/sign_up/app": "./js/sign_up/main.js",
+    "js/login/app": "./js/login/main.js",
+    "js/home_page/app": "./js/home_page/main.js",
+    "js/create_post/app": "./js/create_post/main.js",
+    "js/post/app": "./js/post/main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][hash][ext][query]',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new Dotenv({
+      path: "./.env",
+    }),
+  ],
+}
